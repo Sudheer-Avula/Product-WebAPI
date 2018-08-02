72585636bfc72fea6f5643e73e43a6570b0ff6ce
sudheer.avula <sudheer.avula@NB-SUDHEERA>
2018-08-02 16:45:06 +0530

    Add project files.

---
+++ ProductWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using ProductWebAPI.Data;
+using ProductWebAPI.Model;
+
+namespace ProductWebAPI.Controllers
+{
+      
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginModel model )
+        {
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var calims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub,user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+
+                var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MySuperSecurekey"));
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://oec.com", 
+                    audience: "http://oec.com", 
+                    expires: DateTime.UtcNow.AddHours(1),
+                    claims: calims,
+                    signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                try
+                {
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+
+                    });
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+
+            return Unauthorized();            
+        }
+    }
+}
---
+++ ProductWebAPI/Data/SeedDataBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebAPI.Data
+{
+    public class SeedDataBase
+    {
+        public static void Initilize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+
+            if(!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = "sudheer@live.in",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "Sudheer.Avula"
+                };
+
+                var result = userManager.CreateAsync(user, "Password@123");
+                
+            }
+
+        }
+    }
+}
